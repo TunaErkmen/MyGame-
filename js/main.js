a1de81f74ec4db47bd52f1e6dce726426b259875
TunaErkmen <98965068+TunaErkmen@users.noreply.github.com>
2023-03-05 18:55:19 +0100

    added img, changed css, added function

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const cards = document.querySelectorAll('.memory-card');
+let hasFlippedCard = false ;
+let firstCard, secondCard;
+
+
+
+function flipCard () {
+   this.classList.toggle('flip');
+   if(!hasFlippedCard){
+    //first click
+    hasFlippedCard = true;
+    firstCard= this;
+   } else {
+    //second click
+    hasFlippedCard=false;
+    secondCard= this;
+
+    //do cards match
+    if(firstCard.dataset.framework === secondCard.dataset.framework){
+    }
+   
+   }
+}
+
+
+cards.forEach (card => card.addEventListener('click',flipCard ))
